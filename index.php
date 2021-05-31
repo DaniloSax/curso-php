b1b5ca39e1dc668286a2d3fab9e886488faa82b1
DaniloSax <danilovsdanilo@gmail.com>
2021-05-31 18:01:04 -0400

    add: server xampp + file .php

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    
+
     <title>Curso PHP</title>
 
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    
+
     <link rel="icon" type="image/png" href="./assets/img/favicon.png" />
     <link rel='stylesheet' type='text/css' media='screen' href='./assets/css/style.css'>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <header class="header">
         <h1>Curso de PHP</h1>
@@ -33,7 +35,7 @@
                     </ul>
                 </div>
                 <div class="module red">
-                    <h3>Módulo 02  </h3>
+                    <h3>Módulo 02 </h3>
 
                     <ul>
                         <li><a href="#">Exercício A</a></li>
@@ -42,7 +44,7 @@
                     </ul>
                 </div>
                 <div class="module blue">
-                    <h3>Módulo 03  </h3>
+                    <h3>Módulo 03 </h3>
 
                     <ul>
                         <li><a href="#">Exercício A</a></li>
@@ -51,7 +53,7 @@
                     </ul>
                 </div>
                 <div class="module purple">
-                    <h3>Módulo 04  </h3>
+                    <h3>Módulo 04 </h3>
 
                     <ul>
                         <li><a href="#">Exercício A</a></li>
@@ -60,7 +62,7 @@
                     </ul>
                 </div>
                 <div class="module orange">
-                    <h3>Módulo 05  </h3>
+                    <h3>Módulo 05 </h3>
 
                     <ul>
                         <li><a href="#">Exercício A</a></li>
@@ -69,7 +71,7 @@
                     </ul>
                 </div>
                 <div class="module green-dark-1">
-                    <h3>Módulo 06  </h3>
+                    <h3>Módulo 06 </h3>
 
                     <ul>
                         <li><a href="#">Exercício A</a></li>
@@ -78,7 +80,7 @@
                     </ul>
                 </div>
                 <div class="module red-dark-1">
-                    <h3>Módulo 07  </h3>
+                    <h3>Módulo 07 </h3>
 
                     <ul>
                         <li><a href="#">Exercício A</a></li>
@@ -87,7 +89,7 @@
                     </ul>
                 </div>
                 <div class="module blue-dark-1">
-                    <h3>Módulo 08  </h3>
+                    <h3>Módulo 08 </h3>
 
                     <ul>
                         <li><a href="#">Exercício A</a></li>
@@ -96,7 +98,17 @@
                     </ul>
                 </div>
                 <div class="module purple-dark-1">
-                    <h3>Módulo 09  </h3>
+                    <h3>Módulo 09 </h3>
+
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </div>
+
+                <div class="module purple-dark-1">
+                    <h3>Módulo 10 </h3>
 
                     <ul>
                         <li><a href="#">Exercício A</a></li>
@@ -109,9 +121,10 @@
     </main>
 
     <footer class="footer">
-        COD3R & ALUNOS &copy; 2021
+        COD3R & ALUNOS &copy; <?= date('Y'); ?>
     </footer>
-    
+
     <script src='./assets/js/script.js'></script>
 </body>
+
 </html>
